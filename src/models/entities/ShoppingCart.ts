3d6608c9228bc7d79daa24e31daa8fe11fa933d9
Drew <drewdavis888@gmail.com>
2025-02-18 19:49:07 -0700

    has many model

--- src/models/entities/ShoppingCart.ts
+++
@@ -1,8 +0,0 @@
-import DemoTable from "../DemoTable";
-import { Entity, HasMany, IdAttribute, StringAttribute } from "dyna-record";
-import Order from "./Order";
-
-@Entity
-class ShoppingCart extends DemoTable {}
-
-export default ShoppingCart;
--- src/models/entities/index.ts
+++ src/models/entities/index.ts
@@ -1,3 +1,2 @@
 export * from "./User";
 export * from "./Order";
-export * from "./ShoppingCart";
